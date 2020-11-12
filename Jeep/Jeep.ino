4d0ae9b7074656d6f32c8f217b7b901645d1d18e
mcongdon <mcongdon@gmail.com>
2020-11-12 16:17:47 -0600

    added pwm timer

--- Jeep/Jeep.ino
+++ Jeep/Jeep.ino
@@ -4,7 +4,8 @@
  * Author: Michael Congdon
  *
  */
- 
+
+#include <TimerOne.h>
 
 // pin definitions
 int throttlePin   = A0;     // input pin for throttle pedal
@@ -16,27 +17,31 @@ int ledPin        = 13;     // LED on Arduino Board
 int throttleValue   = 0;
 int throttleMin     = 100;
 int throttleMax     = 860;
-int throttleOutR    = 255; 
-int throttleOutL    = 255; 
+int throttleOutR    = 0; 
+int throttleOutL    = 0; 
+
 
 
 void setup() {
   
   // open serial port connection
   Serial.begin(9600); 
-
-  Serial.println("Setup Started:");
    
   //Init pins
   pinMode(leftMotorPin, OUTPUT);
   pinMode(rightMotorPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
-  
+
   // calibrate min voltage of throttle pedal
-  Serial.println("Throttle Calibration");
-    
   throttleValue = analogRead(throttlePin);
-  throttleMin = throttleValue + 10;
+  throttleMin = throttleValue + 10; 
+
+  // init pwm timer
+  Timer1.initialize(8);// Frequency, 8us = 125KHz
+  
+  // init motors 
+  Timer1.pwm(leftMotorPin, throttleMin);
+  Timer1.pwm(rightMotorPin, throttleMin);
 
   // light indicator pin when setup complete
   Serial.println("Setup Complete:");
@@ -51,20 +56,14 @@ void loop() {
   throttleValue = map(throttleValue, throttleMin, throttleMax, 0, 255);
   // in case the sensor value is outside the range seen during calibration
   throttleValue = constrain(throttleValue, 0, 255);
- 
-  // Convert to 5v voltage reading.
-  //float voltage = sensorValue * (5.0 / 1023.0);
-
-  // invert for resistance output to motor controller. 
-  throttleOutR = (255 - throttleValue); 
-  throttleOutL = (255 - throttleValue); 
 
   // adjust for direction sensor
-  // TODO:
- 
+  throttleOutL = throttleValue; 
+  throttleOutR = throttleValue; 
+
   // output to motors
-  analogWrite(leftMotorPin, throttleOutL);
-  analogWrite(rightMotorPin, throttleOutR);
+  Timer1.pwm(leftMotorPin, throttleOutL);
+  Timer1.pwm(rightMotorPin, throttleOutR);
   
   //delay(2);
   
@@ -72,5 +71,3 @@ void loop() {
   Serial.println(throttleOutR);
  
 }
-
-

7a57fcbc1448875c20eb6db8a5463f95ffda623a
mcongdon <mcongdon@gmail.com>
2020-11-14 15:17:39 -0600

    Update Jeep.ino

--- Jeep/Jeep.ino
+++ Jeep/Jeep.ino
@@ -5,22 +5,31 @@
  *
  */
 
-#include <TimerOne.h>
-
 // pin definitions
-int throttlePin   = A0;     // input pin for throttle pedal
-int leftMotorPin  = 10;     // pwm output left motor
-int rightMotorPin = 11;     // pwm output right motor
+int throttleInputPin   = A0;     // input pin for throttle pedal
+
+int leftMotorForwardIndicatorPin = 0;     // Left Motor is going forward indicator
+int leftMotorReverseIndicatorPin = 1;     // Left Motor is going reverse indicator
+int leftMotorForwardOutputPin    = 2;     // Tell Left Motor to go forward pin
+int leftMotorReverseOutputPin    = 3;     // Tell Left Motor to go reverse pin
+int leftMotorForwardSpeedPin     = 4;     // pwm output left motor Forward Speed
+int leftMotorReverseSpeedPin     = 5;     // pwm output left motor Reverse
+
+
+
 int ledPin        = 13;     // LED on Arduino Board
 
 // throttle calibration
 int throttleValue   = 0;
 int throttleMin     = 100;
 int throttleMax     = 860;
-int throttleOutR    = 0; 
-int throttleOutL    = 0; 
 
+int leftMotorForward    = 0; 
+int leftMotorReverse    = 0; 
+int leftMotorForwardSpeed    = 0; 
+int leftMotorReverseSpeed    = 0; 
 
+bool IsForward = false; 
 
 void setup() {
   
@@ -28,46 +37,76 @@ void setup() {
   Serial.begin(9600); 
    
   //Init pins
-  pinMode(leftMotorPin, OUTPUT);
-  pinMode(rightMotorPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(leftMotorForwardIndicatorPin, OUTPUT);
+  pinMode(leftMotorReverseIndicatorPin, OUTPUT);
+  pinMode(leftMotorForwardOutputPin, OUTPUT);
+  pinMode(leftMotorReverseOutputPin, OUTPUT);
+  pinMode(leftMotorForwardSpeedPin, OUTPUT);
+  pinMode(leftMotorReverseSpeedPin, OUTPUT);
+
+  // init Left motor (apply parking brake) 
+  digitalWrite(leftMotorForwardOutputPin, leftMotorForward); 
+  digitalWrite(leftMotorReverseOutputPin, leftMotorReverse);   
+  analogWrite(leftMotorForwardSpeedPin, leftMotorForwardSpeed);
+  analogWrite(leftMotorReverseSpeedPin, leftMotorReverseSpeed);
 
   // calibrate min voltage of throttle pedal
   throttleValue = analogRead(throttlePin);
   throttleMin = throttleValue + 10; 
-
-  // init pwm timer
-  Timer1.initialize(8);// Frequency, 8us = 125KHz
   
-  // init motors 
-  Timer1.pwm(leftMotorPin, throttleMin);
-  Timer1.pwm(rightMotorPin, throttleMin);
 
-  // light indicator pin when setup complete
+  // light indicator pin when setup complete 
   Serial.println("Setup Complete:");
-  digitalWrite(ledPin, HIGH);
-}
+
+  // TODO need to have a forward back button of some sort. TBD. 
+  IsForward = true;
+  }
 
 void loop() {
 
   // read throttle pedal
   throttleValue = analogRead(throttlePin);
+  
   // apply the calibration to the sensor reading
-  throttleValue = map(throttleValue, throttleMin, throttleMax, 0, 255);
+  //throttleValue = map(throttleValue, throttleMin, throttleMax, 0, 255);
   // in case the sensor value is outside the range seen during calibration
-  throttleValue = constrain(throttleValue, 0, 255);
+  //throttleValue = constrain(throttleValue, 0, 255);
 
-  // adjust for direction sensor
-  throttleOutL = throttleValue; 
-  throttleOutR = throttleValue; 
 
+  // Need to handle reverse... 
+  if(throttleValue > 20 ) 
+  {
+    IsForward = true; 
+  } else {
+    
+    IsForward = false; 
+  }
+  
+  
+  // Apply direction var to motor controll 
+  if(IsForward) {
+    leftMotorForwardSpeed = throttleValue;
+    leftMotorReverseSpeed = 0; 
+    leftMotorForward = 1;
+    leftMotorReverse = 0;  
+  } else {
+    // apply brakes 
+    leftMotorForwardSpeed = 0;
+    leftMotorReverseSpeed = 0; 
+    leftMotorForward = 0;
+    leftMotorReverse = 0;     
+  }
+
+   
   // output to motors
-  Timer1.pwm(leftMotorPin, throttleOutL);
-  Timer1.pwm(rightMotorPin, throttleOutR);
+  digitalWrite(leftMotorForwardOutputPin, leftMotorForward); 
+  digitalWrite(leftMotorReverseOutputPin, leftMotorReverse);   
+  analogWrite(leftMotorForwardSpeedPin, leftMotorForwardSpeed);
+  analogWrite(leftMotorReverseSpeedPin, leftMotorReverseSpeed);
   
-  //delay(2);
+  delay(10);
   
-  Serial.println(throttleOutL);
-  Serial.println(throttleOutR);
+  Serial.println(throttleValue);
+
  
 }

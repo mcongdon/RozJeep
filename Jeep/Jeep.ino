f37deec01dcc26568ef92e00adad2521e11941a9
mcongdon <mcongdon@gmail.com>
2020-11-19 08:34:29 -0600

    added ping logic

--- Jeep/Jeep.ino
+++ Jeep/Jeep.ino
@@ -37,8 +37,8 @@ int leftMotorReverseSpeed     = 0;
 int rightMotorForwardSpeed    = 0; 
 int rightMotorReverseSpeed    = 0; 
 
-
-bool IsForward = false; 
+// initial state stopped
+bool MoveForward = false; 
 
 void setup() {
   
@@ -79,14 +79,34 @@ void setup() {
   // calibrate min throttle pedal
   throttleValue = analogRead(throttleInputPin);
   throttleMin = throttleValue + 10; 
-  
-  // TODO need to have a forward back button of some sort. TBD. 
-  IsForward = true;
-  
+    
 }
 
 void loop() {
 
+  
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  long pingDuration, pingInches;
+  pinMode(pingInputPin, OUTPUT);
+  digitalWrite(pingInputPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingInputPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingInputPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH pulse
+  // whose duration is the time (in microseconds) from the sending of the ping
+  // to the reception of its echo off of an object.
+  pinMode(pingInputPin, INPUT);
+  pingDuration = pulseIn(pingInputPin, HIGH);
+
+  // convert the time into a distance
+  pingInches = microsecondsToInches(pingDuration);
+
+  Serial.println(inches); 
+
+  
   // read throttle pedal
   throttleValue = analogRead(throttleInputPin);
   // apply the calibration to the sensor reading
@@ -95,18 +115,19 @@ void loop() {
   throttleValue = constrain(throttleValue, 0, 255);
 
 
+  //Resolve direction and collision sensors 
   // Need to handle reverse... 
-  if(throttleValue > 80 ) 
+  if(throttleValue > 80 && pingInches > 2) 
   {
-    IsForward = true; 
+    MoveForward = true; 
   } else {
     
-    IsForward = false; 
+    MoveForward = false; 
   }
   
   
   // Apply direction var to motor control
-  if(IsForward) {
+  if(MoveForward) {
     
     // set forward speed
     leftMotorForwardSpeed   = throttleValue;
@@ -141,9 +162,22 @@ void loop() {
   analogWrite(leftMotorReverseSpeedPin, leftMotorReverseSpeed);
   analogWrite(rightMotorForwardSpeedPin, rightMotorForwardSpeed);
   analogWrite(rightMotorReverseSpeedPin, rightMotorReverseSpeed);
-  
-  delay(2);
-  
-  int ping = digitalRead(pingInputPin);
-  Serial.println(ping); 
+    
+}
+
+
+long microsecondsToInches(long microseconds) {
+  // According to Parallax's datasheet for the PING))), there are 73.746
+  // microseconds per inch (i.e. sound travels at 1130 feet per second).
+  // This gives the distance travelled by the ping, outbound and return,
+  // so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds / 29 / 2;
 }

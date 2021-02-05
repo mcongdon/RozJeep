59439535a1808973f9ae341a1198a3f7eb6f3996
mcongdon <mcongdon@gmail.com>
2021-02-04 18:40:01 -0600

    updated to include basic throttle easing
    to prevent hard stop etc

--- Jeep/Jeep.ino
+++ Jeep/Jeep.ino
@@ -11,25 +11,25 @@ int throttleInputPin    = A0;     // input pin for throttle pedal
 
 int relayPin          = 11;                 // output pin for aux power relay 
 
-int reversePin                  = 2;        // reverse switch pin
+int backwardsPin                = 2;        // reverse switch pin
 int rightWheelTurningPin        = 3;        // pin for right (wheel) turning sensor if true turning left
 int leftWheelTurningPin         = 4;        // pin for left (wheel) turning sensor if true turning right
 
 int safteyInputPin              = 52;       // input pin for sensor board
 
 int leftMotorForwardIndicatorPin = 26;      // Left Motor is going forward indicator
-int leftMotorReverseIndicatorPin = 27;      // Left Motor is going reverse indicator
+int leftMotorBackwardsIndicatorPin = 27;      // Left Motor is going reverse indicator
 int leftMotorForwardOutputPin    = 28;      // Tell Left Motor to go forward pin
-int leftMotorReverseOutputPin    = 29;      // Tell Left Motor to go reverse pin
+int leftMotorBackwardsOutputPin    = 29;      // Tell Left Motor to go reverse pin
 int leftMotorForwardSpeedPin     = 9;      // pwm output left motor Forward Speed
-int leftMotorReverseSpeedPin     = 10;       // pwm output left motor Reverse
+int leftMotorBackwardsSpeedPin     = 10;       // pwm output left motor Reverse
 
 int rightMotorForwardIndicatorPin = 22;      // Right Motor is going forward indicator
-int rightMotorReverseIndicatorPin = 23;      // Right Motor is going reverse indicator
+int rightMotorBackwardsIndicatorPin = 23;      // Right Motor is going reverse indicator
 int rightMotorForwardOutputPin    = 24;      // Tell Right Motor to go forward pin
-int rightMotorReverseOutputPin    = 25;      // Tell Right Motor to go reverse pin
+int rightMotorBackwardsOutputPin    = 25;      // Tell Right Motor to go reverse pin
 int rightMotorForwardSpeedPin     = 5;       // pwm output right motor Forward Speed
-int rightMotorReverseSpeedPin     = 6;       // pwm output right motor Reverse
+int rightMotorBackwardsSpeedPin     = 6;       // pwm output right motor Reverse
 
 
 // throttle calibration
@@ -41,12 +41,20 @@ long leftMotorAdjust = 1;     // factor to reduce speed during turn
 long rightMotorAdjust = 1;    // factor to reduce speed during turn 
 
 int leftMotorForwardSpeed     = 0; 
-int leftMotorReverseSpeed     = 0; 
+int leftMotorBackwardsSpeed     = 0; 
 int rightMotorForwardSpeed    = 0; 
-int rightMotorReverseSpeed    = 0; 
+int rightMotorBackwardsSpeed    = 0; 
+
+int SpeedDeltaThreshold = 50;
+
 
 // initial state stopped
 bool IsMoving = false; 
+bool IsMovingForward = false; 
+bool IsMovingBackwards = false; 
+bool IsTurningRight = false; 
+bool IsTurningLeft = false; 
+
 bool SafteyPass = false; 
 bool DebugMode = true; 
 
@@ -60,7 +68,7 @@ void setup() {
   
   //Init sensor pins
   pinMode(throttleInputPin, INPUT);
-  pinMode(reversePin, INPUT);
+  pinMode(backwardsPin, INPUT);
   pinMode(rightWheelTurningPin, INPUT);
   pinMode(leftWheelTurningPin, INPUT);
   pinMode(safteyInputPin, INPUT);  
@@ -68,30 +76,30 @@ void setup() {
   
   //Init motor pins    
   pinMode(leftMotorForwardIndicatorPin, INPUT);
-  pinMode(leftMotorReverseIndicatorPin, INPUT);
+  pinMode(leftMotorBackwardsIndicatorPin, INPUT);
   pinMode(rightMotorForwardIndicatorPin, INPUT);
-  pinMode(rightMotorReverseIndicatorPin, INPUT);
+  pinMode(rightMotorBackwardsIndicatorPin, INPUT);
 
   pinMode(leftMotorForwardOutputPin, OUTPUT);
-  pinMode(leftMotorReverseOutputPin, OUTPUT);
+  pinMode(leftMotorBackwardsOutputPin, OUTPUT);
   pinMode(rightMotorForwardOutputPin, OUTPUT);
-  pinMode(rightMotorReverseOutputPin, OUTPUT);
+  pinMode(rightMotorBackwardsOutputPin, OUTPUT);
   
   pinMode(leftMotorForwardSpeedPin, OUTPUT);
-  pinMode(leftMotorReverseSpeedPin, OUTPUT);
+  pinMode(leftMotorBackwardsSpeedPin, OUTPUT);
   pinMode(rightMotorForwardSpeedPin, OUTPUT);
-  pinMode(rightMotorReverseSpeedPin, OUTPUT);
+  pinMode(rightMotorBackwardsSpeedPin, OUTPUT);
 
   // init motor (apply parking brake) 
   digitalWrite(leftMotorForwardOutputPin, LOW); 
-  digitalWrite(leftMotorReverseOutputPin, LOW);
+  digitalWrite(leftMotorBackwardsOutputPin, LOW);
   digitalWrite(rightMotorForwardOutputPin, LOW); 
-  digitalWrite(rightMotorReverseOutputPin, LOW);
+  digitalWrite(rightMotorBackwardsOutputPin, LOW);
      
   analogWrite(leftMotorForwardSpeedPin, leftMotorForwardSpeed);
-  analogWrite(leftMotorReverseSpeedPin, leftMotorReverseSpeed);
+  analogWrite(leftMotorBackwardsSpeedPin, leftMotorBackwardsSpeed);
   analogWrite(rightMotorForwardSpeedPin, rightMotorForwardSpeed);
-  analogWrite(rightMotorReverseSpeedPin, rightMotorReverseSpeed);
+  analogWrite(rightMotorBackwardsSpeedPin, rightMotorBackwardsSpeed);
 
   // calibrate min throttle pedal
   throttleValue = analogRead(throttleInputPin);
@@ -100,9 +108,6 @@ void setup() {
   // turn on power to dashboard 
   digitalWrite(relayPin, HIGH); 
   
-  if(DebugMode){
-    Serial.println("**STARTUP END****");    
-  } 
 }
 
 void loop() {
@@ -111,27 +116,18 @@ void loop() {
   /*-------------------------------------------------------------*/
   // read throttle pedal
   throttleValue = analogRead(throttleInputPin);
-  if(DebugMode){
-    Serial.print("Throttle RAW: ");
-    Serial.print(throttleValue);
-    Serial.print(",");
-  }
+  
+  // if current throttle reading is less than min reset min. 
+  //if(throttleMin > trottleValue ){    
+   // throttleMin = throttleValue; 
+  //}
+  
   // apply the calibration to the sensor reading
   throttleValue = map(throttleValue, throttleMin, throttleMax, 0, 255);
-  if(DebugMode){
-    Serial.print("Throttle Map: ");
-    Serial.print(throttleValue);
-    Serial.print(",");
-  }
   
   // in case the sensor value is outside the range seen during calibration
   throttleValue = constrain(throttleValue, 0, 255);
-  if(DebugMode){
-    Serial.print("Throttle Constrain: ");
-    Serial.print(throttleValue);
-    Serial.print(" : ");
-  }  
-
+ 
   
   //Saftey sensors
   /*-------------------------------------------------------------*/
@@ -141,153 +137,224 @@ void loop() {
   // TODO need to filter for random interfance- SaftyPass == false will hard stop the Jeep- motors off. 
   //SafteyPass = digitalRead(safteyInputPin); 
  
-  SafteyPass = true; 
-
+  SafteyPass = true;   
   
-  // This needs to be more sophisticated...
-  // IsMoving = (throttleValue > 50);   
-  IsMoving = true;
+  // resolve direction states
+  IsMoving            = resolveIsMovingState();
+  IsMovingForward     = resolveIsMovingForwardState(); 
+  IsMovingBackwards   = resolveIsMovingBackwardsState(); 
+  IsTurningRight      = resolveIsTurningRightState(); 
+  IsTurningLeft       = resolveIsTurningLeftState(); 
 
-  
-  //Motor Control - find speed value for each motor
+  // resolve speed and directions 
+  leftMotorForwardSpeed     = resolveLeftMotorForwardSpeed(); 
+  leftMotorBackwardsSpeed   = resolveLeftMotorBackwardsSpeed();
+  rightMotorForwardSpeed    = resolveRightMotorForwardSpeed();
+  rightMotorBackwardsSpeed  = resolveRightMotorBackwardsSpeed();
+
+  // Saftey Check
   /*-------------------------------------------------------------*/
-  
-  if(IsMoving) {
-
-    if(DebugMode){
-      Serial.print("Is Moving: ");
-    }  
-
-
-
-    // Resolve Direction Sensors
-    /*-------------------------------------------------------------*/
-    // ** note the mechanical design prevents both right wheel sensor and left wheel sensor to both be true at the same time
-    // ** exception should be created and handled for such an event, indicating credical mechanical failure 
+  if(!SafteyPass){
     
-    // if right wheel sensor active jeep is turning left. 
-    if(digitalRead(rightWheelTurningPin)){
-
-      if(DebugMode){
-        Serial.print("LEFT AND ");
-      }        
-      // is turning left - slow left motor 
-      leftMotorAdjust = .7; 
-
-    } else {
-      
-      // not going left
-      leftMotorAdjust = 1; 
-    }
-  
-    // left wheel sensor active jeep is turning right. 
-    if(digitalRead(leftWheelTurningPin)){
-       
-       if(DebugMode){
-        Serial.print("RIGHT AND ");
-      }                 
-      // is turning right - slow right motor 
-      rightMotorAdjust = .7; 
-
-    } else {
-      
-      // not going right
-      rightMotorAdjust = 1; 
-    }
-
-    
-    // Resolve Forward/Reverse
-    /*-------------------------------------------------------------*/
-    // if reverse sensor active Jeep is moving backwards
-    if (!digitalRead(reversePin)){
-
-      if(DebugMode){
-        Serial.println("BACKWARDS");
-      }  
-
-      // is moving backwards    
-      leftMotorReverseSpeed   = (throttleValue * leftMotorAdjust);
-      leftMotorForwardSpeed   = 0; 
-    
-      rightMotorReverseSpeed  = (throttleValue * rightMotorAdjust);
-      rightMotorForwardSpeed  = 0; 
-
-   
-    } else {
-
-      if(DebugMode){
-        Serial.println("FORWARD");
-      }  
-      
-      // is moving forward
-      leftMotorForwardSpeed   = (throttleValue * leftMotorAdjust);
-      leftMotorReverseSpeed   = 0; 
-    
-      rightMotorForwardSpeed  = (throttleValue * rightMotorAdjust);
-      rightMotorReverseSpeed  = 0; 
-    }
-    
-    if(DebugMode){    
-      Serial.println("************");
-      Serial.println("************");
-      Serial.println("RAW rightwheel : " + digitalRead(rightWheelTurningPin)); 
-      Serial.println("RAW leftwheel : " + digitalRead(leftWheelTurningPin));
-      Serial.println("RAW reverse : " + digitalRead(reversePin));  
-      Serial.println("************");
-      Serial.println("************");
-    }
-    
-  } else { 
-
-
+    // error log 
     if(DebugMode){
       Serial.println("************");
       Serial.println("************");
-      Serial.println("************");
-      Serial.println("*** IS NOT MOVING *** ");
-      Serial.println("************");
+      Serial.println("*** Saftey Error *** ");
       Serial.println("************");
       Serial.println("************");
-    }  
-    
-    // Stop Jeep (gently)
-    /*-------------------------------------------------------------*/
-        
-    // zero speed values 
-    leftMotorForwardSpeed = 0;
-    leftMotorReverseSpeed = 0; 
-    rightMotorForwardSpeed = 0;
-    rightMotorReverseSpeed = 0; 
-    
-  
-  } // end IsMoving IF
-
-
-  // Move Jeep
-  /*-------------------------------------------------------------*/
-
-  if(SafteyPass){
-  
-    // enable motors 
-    digitalWrite(leftMotorForwardOutputPin, HIGH); 
-    digitalWrite(leftMotorReverseOutputPin, HIGH);   
-    digitalWrite(rightMotorForwardOutputPin, HIGH); 
-    digitalWrite(rightMotorReverseOutputPin, HIGH);          
-  } else {
+    }
     
     // disable motors 
     digitalWrite(leftMotorForwardOutputPin, LOW); 
-    digitalWrite(leftMotorReverseOutputPin, LOW);   
+    digitalWrite(leftMotorBackwardsOutputPin, LOW);   
     digitalWrite(rightMotorForwardOutputPin, LOW); 
-    digitalWrite(rightMotorReverseOutputPin, LOW);          
-  }
+    digitalWrite(rightMotorBackwardsOutputPin, LOW);    
 
-    
-  // apply speed
-  analogWrite(leftMotorForwardSpeedPin, leftMotorForwardSpeed);
-  analogWrite(leftMotorReverseSpeedPin, leftMotorReverseSpeed);
-  analogWrite(rightMotorForwardSpeedPin, rightMotorForwardSpeed);
-  analogWrite(rightMotorReverseSpeedPin, rightMotorReverseSpeed);
+    // HARD STOP
+    leftMotorForwardSpeed   = 0; 
+    leftMotorBackwardsSpeed   = 0;
+    rightMotorForwardSpeed  = 0;
+    rightMotorBackwardsSpeed  = 0;
+
+    analogWrite(leftMotorForwardSpeedPin, leftMotorForwardSpeed);
+    analogWrite(leftMotorBackwardsSpeedPin, leftMotorBackwardsSpeed);
+    analogWrite(rightMotorForwardSpeedPin, rightMotorForwardSpeed);
+    analogWrite(rightMotorBackwardsSpeedPin, rightMotorBackwardsSpeed);    
 
   
+  } else {
+    
+    // Move Jeep
+    /*-------------------------------------------------------------*/    
+
+    // enable motors
+    digitalWrite(leftMotorForwardOutputPin, HIGH); 
+    digitalWrite(leftMotorBackwardsOutputPin, HIGH);   
+    digitalWrite(rightMotorForwardOutputPin, HIGH); 
+    digitalWrite(rightMotorBackwardsOutputPin, HIGH);                      
+    
+    // apply speed
+    analogWrite(leftMotorForwardSpeedPin, leftMotorForwardSpeed);
+    analogWrite(leftMotorBackwardsSpeedPin, leftMotorBackwardsSpeed);
+    analogWrite(rightMotorForwardSpeedPin, rightMotorForwardSpeed);
+    analogWrite(rightMotorBackwardsSpeedPin, rightMotorBackwardsSpeed);    
+  }
+  
 }// loop 
+
+
+int resolveLeftMotorForwardSpeed()
+{
+  if(IsMovingForward)
+  {
+    if(leftMotorBackwardsSpeed == 0){
+      int newSpeed = int(throttleValue * leftMotorAdjust); 
+      return  easeThrottleChange(leftMotorForwardSpeed, newSpeed);
+    } else {
+     // wait for reverse speed to get to zero
+      return 0; 
+    }
+  } else if (IsMovingBackwards && leftMotorForwardSpeed > 0) {
+    
+     // if trying to go backwards reduce forward speed to 0; 
+     return  easeThrottleChange(leftMotorForwardSpeed, 0);   
+    
+  }
+
+  return 0; 
+}     
+
+
+int resolveLeftMotorBackwardsSpeed()
+{  
+  if(IsMovingBackwards)
+  {
+      if(leftMotorForwardSpeed == 0){
+        int newSpeed = int(throttleValue * leftMotorAdjust); 
+        return  easeThrottleChange(leftMotorBackwardsSpeed, newSpeed);
+      } else {
+        // wait for forward speed to get to zero 
+        return 0; 
+      }
+  } else if (IsMovingForward && leftMotorBackwardsSpeed > 0) {
+    
+     // if trying to go backwards reduce forward speed to 0; 
+     return  easeThrottleChange(leftMotorBackwardsSpeed, 0);   
+    
+  }
+    return 0;
+}
+
+int resolveRightMotorForwardSpeed()
+{
+  if(IsMovingForward)
+  {
+    if(rightMotorBackwardsSpeed == 0){
+      int newSpeed = int(throttleValue * rightMotorAdjust); 
+      return  easeThrottleChange(rightMotorForwardSpeed, newSpeed);
+    } else {
+     // wait for reverse speed to get to zero
+      return 0; 
+    }
+  } else if (IsMovingBackwards && rightMotorForwardSpeed > 0) {
+    
+     // if trying to go backwards reduce forward speed to 0; 
+     return  easeThrottleChange(rightMotorForwardSpeed, 0);   
+    
+  }
+
+  return 0; 
+}
+
+int resolveRightMotorBackwardsSpeed()
+{
+    if(IsMovingBackwards)
+  {
+      if(rightMotorForwardSpeed == 0){
+        int newSpeed = int(throttleValue * rightMotorAdjust); 
+        return  easeThrottleChange(rightMotorBackwardsSpeed, newSpeed);
+      } else {
+        // wait for forward speed to get to zero 
+        return 0; 
+      }
+  } else if (IsMovingForward && rightMotorBackwardsSpeed > 0) {
+    
+     // if trying to go backwards reduce forward speed to 0; 
+     return  easeThrottleChange(rightMotorBackwardsSpeed, 0);   
+    
+  }
+
+  return 0;
+}
+
+
+
+int easeThrottleChange(int oldSpeed, int newSpeed){
+    
+    if (abs(oldSpeed - newSpeed) > SpeedDeltaThreshold) 
+    {
+       if(oldSpeed > newSpeed) {
+        return (oldSpeed - SpeedDeltaThreshold); 
+       } else if (newSpeed > oldSpeed){
+        return (oldSpeed + SpeedDeltaThreshold); 
+       }
+    } 
+
+    return newSpeed;
+}
+
+
+bool resolveIsMovingState(){
+   
+  // This needs to be more sophisticated...
+  // IsMoving = (throttleValue > 50);   
+   // do throttle test here    
+   return true; 
+}
+
+bool resolveIsMovingForwardState(){
+  if(!IsMoving) {return false;}
+  
+  // if reverse is active
+  if(resolveIsMovingBackwardsState()){
+    return false; 
+  }
+  
+  return true;
+}
+
+bool resolveIsMovingBackwardsState(){
+  if(!IsMoving) {return false;}
+
+  // if reverse pin 
+  if(!digitalRead(backwardsPin)){
+    return true;
+  }   
+
+  return false; 
+}
+
+bool resolveIsTurningRightState(){
+  if(!IsMoving) {return false;} 
+  
+  if(!digitalRead(leftWheelTurningPin) && digitalRead(rightWheelTurningPin)){
+    rightMotorAdjust = .7; 
+    return true;
+  }
+  
+  rightMotorAdjust = 1; 
+  return false;
+}
+
+bool resolveIsTurningLeftState(){
+  if(!IsMoving) {return false;}
+  
+  if(!digitalRead(rightWheelTurningPin) && digitalRead(leftWheelTurningPin)){
+    leftMotorAdjust = .7;
+    return true; 
+  }
+  
+  leftMotorAdjust = 1; 
+  return false;    
+}
